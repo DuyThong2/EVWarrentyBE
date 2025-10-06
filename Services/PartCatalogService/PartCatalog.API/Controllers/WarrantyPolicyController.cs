29227b9e6d2384229c1ce29331f8e0a7fab2673a
NTVux <nguyentrivu2016@gmail.com>
2025-10-07 00:07:57 +0700

    Add CRUD for WarrantyPolicy

---
+++ Services/PartCatalogService/PartCatalog.API/Controllers/WarrantyPolicyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartCatalog.API.Controllers
+{
+    public class WarrantyPolicyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
