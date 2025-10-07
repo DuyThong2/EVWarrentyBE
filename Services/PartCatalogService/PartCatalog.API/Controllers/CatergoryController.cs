93e531acb2faedf724f3ae4503eb9fe557297ec7
NTVux <nguyentrivu2016@gmail.com>
2025-10-07 09:24:52 +0700

    Finish CRUD and controller not test yet

---
+++ Services/PartCatalogService/PartCatalog.API/Controllers/CatergoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartCatalog.API.Controllers
+{
+    public class CatergoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
