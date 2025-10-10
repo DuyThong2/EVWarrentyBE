5986ab5b59701ee7899ead13122ec14213b896d9
NTVux <nguyentrivu2016@gmail.com>
2025-10-10 08:57:22 +0700

    Add GetPartBySerialNumber function

---
+++ Services/PartCatalogService/PartCatalog.Application/CQRS/Queries/GetPartBySerialNumber/GetPartBySerialNumberQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartCatalog.Application.CQRS.Queries.GetPartBySerialNumber
+{
+    internal class GetPartBySerialNumberQuery
+    {
+    }
+}
